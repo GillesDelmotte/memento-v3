fda308605b2683d8bee6b4053efc0fb9c1af8fbc
gilles delmotte <gilles.delmotte@outlook.be>
2020-04-17 18:19:38 +0200

    add some user in seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -28,8 +28,43 @@ class UsersTableSeeder extends Seeder
         ]);
         App\User::create([
             'name' => 'Véronique Dubois',
-            'email' => 'vernonique.dubois@outlook.be',
+            'email' => 'veronique.dubois@outlook.be',
             'address' => 'Rue Fraischamps 154, 4030 grivegnée',
+            'gsm' => '0477 90 52 29',
+            'password' => Hash::make('azerty'),
+        ]);
+        App\User::create([
+            'name' => 'Jean Charles',
+            'email' => 'jean.charles@outlook.be',
+            'address' => 'Rue de la bonne fortune 324, 4000 Liège',
+            'gsm' => '0434 56 32 45',
+            'password' => Hash::make('azerty'),
+        ]);
+        App\User::create([
+            'name' => 'Martin Collard',
+            'email' => 'martin.Collard@outlook.be',
+            'address' => 'Rue du paradis 456, 4030 grivegnée',
+            'gsm' => '0449 50 38 28',
+            'password' => Hash::make('azerty'),
+        ]);
+        App\User::create([
+            'name' => 'Romain Messana',
+            'email' => 'romain.messana@outlook.be',
+            'address' => 'Rue de la campagne 34, 4020 Chênée',
+            'gsm' => '0439 40 29 10',
+            'password' => Hash::make('azerty'),
+        ]);
+        App\User::create([
+            'name' => 'Brice Myklos',
+            'email' => 'brice.myklos@outlook.be',
+            'address' => 'Rue de la belle canne 490, 4309 Ninane',
+            'gsm' => '0429 48 10 30',
+            'password' => Hash::make('azerty'),
+        ]);
+        App\User::create([
+            'name' => 'Charlotte Toussaint',
+            'email' => 'charlotte.toussaint@outlook.be',
+            'address' => 'Rue Fraischamps 154, 0987 luxembourg ville',
             'gsm' => '0477 90 52 29',
             'password' => Hash::make('azerty'),
         ]);
