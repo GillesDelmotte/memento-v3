b68e6a07ca9ea329bd876f3985ba075cb90e63f4
gilles delmotte <gilles.delmotte@outlook.be>
2020-04-20 17:15:37 +0200

    create filter for practitionners

--- resources/js/components/Search.vue
+++ resources/js/components/Search.vue
@@ -1,8 +1,27 @@
 <template>
   <div>
-    <ul class="userCards">
-      <user-card v-for="user in allPractitioner" :key="user.id" :user="user"></user-card>
+    <ul class="userCards" v-if="filteredBy === 'name'">
+      <user-card v-for="user in filteredByName" :key="user.id" :user="user"></user-card>
     </ul>
+    <ul class="userCards" v-if="filteredBy === 'job'">
+      <user-card v-for="user in filteredByJob" :key="user.id" :user="user"></user-card>
+    </ul>
+    <div class="filter">
+      <h2 class="filter__title">Filter par</h2>
+      <div class="filter__name__job">
+        <div class="filter__name active" @click="changeFilter('name')">Nom</div>
+        <div class="filter__job" @click="changeFilter('job')">Profession</div>
+      </div>
+      <div class="filter__alphabet"></div>
+      <input
+        type="text"
+        name="filter"
+        class="filter__input"
+        id="filter"
+        v-model="filter"
+        autocomplete="off"
+      />
+    </div>
   </div>
 </template>
 <script>
@@ -14,10 +33,49 @@ import { mapState } from "vuex";
 export default {
   name: "Search",
   data() {
-    return {};
+    return {
+      alphabet: "abcdefghijklmnopqrstuvwxyz",
+      filteredBy: "name",
+      filter: ""
+    };
+  },
+  methods: {
+    changeFilter(type) {
+      const items = document.querySelectorAll(".filter__name__job div");
+      items.forEach(item => {
+        item.classList.remove("active");
+      });
+      const newFocus = document
+        .querySelector(".filter__" + type)
+        .classList.add("active");
+
+      this.filteredBy = type;
+    }
   },
   computed: {
-    ...mapState(["allJob", "allPractitioner"])
+    ...mapState(["allJob", "allPractitioner"]),
+    filteredByName() {
+      if (this.filter.length === 0) {
+        return this.allPractitioner;
+      } else {
+        return this.allPractitioner.filter(practitioner => {
+          return practitioner.name
+            .toLowerCase()
+            .match(this.filter.toLowerCase());
+        });
+      }
+    },
+    filteredByJob() {
+      if (this.filter.length === 0) {
+        return this.allPractitioner;
+      } else {
+        return this.allPractitioner.filter(practitioner => {
+          return practitioner.job.name
+            .toLowerCase()
+            .match(this.filter.toLowerCase());
+        });
+      }
+    }
   },
   mounted() {
     this.$store.dispatch("setAllJob").then(() => {
--- resources/js/components/UserCard.vue
+++ resources/js/components/UserCard.vue
@@ -4,6 +4,29 @@
     <div class="userCard__name">{{user.name}}</div>
     <div class="userCard__job">{{user.job.name}}</div>
     <div class="useCard__address">{{user.address}}</div>
+    <div class="userCard__icons">
+      <a
+        href
+        :title="'aller sur l‘horaire de ' + user.name"
+        class="userCard__icons__link userCard__icons__link--schedule"
+      >
+        <span class="sr-only">aller sur l'horaire de {{user.name}}</span>
+      </a>
+      <a
+        href
+        :title="'envoyer un email à ' + user.name"
+        class="userCard__icons__link userCard__icons__link--mail"
+      >
+        <span class="sr-only">envoyer un email à {{user.name}}</span>
+      </a>
+      <a
+        href
+        :title="'téléphoner a ' + user.name"
+        class="userCard__icons__link userCard__icons__link--phone"
+      >
+        <span class="sr-only">téléphoné à {{user.name}}</span>
+      </a>
+    </div>
   </li>
 </template>
 <script>
