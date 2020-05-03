67ce40c0399e8da7112c5bd9f2f46272e1070c34
gilles delmotte <gilles.delmotte@outlook.be>
2020-05-03 17:49:38 +0200

    repsonsive for navigation

--- resources/js/components/HeaderComponent.vue
+++ resources/js/components/HeaderComponent.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="header">
+    <div class="header__logo">M</div>
     <div class="header__title">{{title}}</div>
   </div>
 </template>
--- resources/js/components/Navigation.vue
+++ resources/js/components/Navigation.vue
@@ -1,29 +1,35 @@
 <template>
-  <div class="nav">
-    <div class="nav__logo">M</div>
-    <div class="nav__links">
-      <router-link :to="{name:'meet'}" active-class="active" class="nav__link meet">
-        <i class="icon"></i>
-        <span>Rendez-vous</span>
-      </router-link>
-      <router-link :to="{name:'search'}" active-class="active" class="nav__link praticiens">
-        <i class="icon"></i>
-        <span>Praticiens</span>
-      </router-link>
-      <router-link :to="{name:'schedule'}" active-class="active" class="nav__link schedule">
-        <i class="icon"></i>
-        <span>Horaire</span>
-      </router-link>
-      <router-link :to="{name:'user'}" active-class="active" class="nav__link user">
-        <i class="icon"></i>
-        <span>Profil</span>
-      </router-link>
-      <a @click="logout" ref="logout" shref class="nav__link disconnect">
-        <i class="icon"></i>
-        <span>Déconnexion</span>
-      </a>
+  <div>
+    <div class="nav">
+      <div class="nav__links">
+        <router-link :to="{name:'meet'}" active-class="active" class="nav__link meet">
+          <i class="icon" @click="closeResponsiveMenu"></i>
+          <span @click="closeResponsiveMenu">Rendez-vous</span>
+        </router-link>
+        <router-link :to="{name:'search'}" active-class="active" class="nav__link praticiens">
+          <i class="icon" @click="closeResponsiveMenu"></i>
+          <span @click="closeResponsiveMenu">Praticiens</span>
+        </router-link>
+        <router-link :to="{name:'schedule'}" active-class="active" class="nav__link schedule">
+          <i class="icon" @click="closeResponsiveMenu"></i>
+          <span @click="closeResponsiveMenu">Horaire</span>
+        </router-link>
+        <router-link :to="{name:'user'}" active-class="active" class="nav__link user">
+          <i class="icon" @click="closeResponsiveMenu"></i>
+          <span @click="closeResponsiveMenu">Profil</span>
+        </router-link>
+        <a @click="logout" ref="logout" shref class="nav__link disconnect">
+          <i class="icon"></i>
+          <span>Déconnexion</span>
+        </a>
+      </div>
+      <div class="nav__burger" @click="openMenu">
+        <span class="top"></span>
+        <span class="mid"></span>
+        <span class="bottom"></span>
+      </div>
     </div>
-    <div class="nav__burger" @click="openMenu">
+    <div class="responsive__burger" @click="responsiveOpenMenu">
       <span class="top"></span>
       <span class="mid"></span>
       <span class="bottom"></span>
@@ -50,6 +56,18 @@ export default {
     closeMenu() {
       const nav = document.querySelector(".nav");
       nav.classList.remove("open");
+    },
+    responsiveOpenMenu() {
+      const nav = document.querySelector(".nav");
+      if (nav.classList.contains("responsive__open")) {
+        nav.classList.remove("responsive__open");
+      } else {
+        nav.classList.add("responsive__open");
+      }
+    },
+    closeResponsiveMenu() {
+      const nav = document.querySelector(".nav");
+      nav.classList.remove("responsive__open");
     }
   }
 };
